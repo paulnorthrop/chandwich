41fd1c571b891ff9e385635b5d42ef268c857aad
Paul Northrop <p.northrop@ucl.ac.uk>
2017-10-01 16:44:55 +0100

    Add test based on algebraic derivaties.

--- tests/testthat/test-compare_to_sandwich.R
+++ tests/testthat/test-compare_to_sandwich.R
@@ -38,3 +38,36 @@ test_that("SEs agree", {
 test_that("adjusted SEs agree", {
   testthat::expect_equal(adj_ses, my_adj_ses, tolerance = my_tol)
 })
+
+# Repeat using algebraic derivatives and Hessian
+
+pois_alg_deriv <- function(pars, y, x) {
+  mu <- exp(pars[1] + pars[2] * x + pars[3] * x ^ 2)
+  return(cbind(y - mu, x * (y - mu), x ^2 * (y - mu)))
+}
+
+pois_alg_hess <- function(pars, y, x) {
+  mu <- exp(pars[1] + pars[2] * x + pars[3] * x ^ 2)
+  alg_hess <- matrix(0, 3, 3)
+  alg_hess[1, ] <- -c(sum(mu), sum(x * mu), sum(x ^ 2 * mu))
+  alg_hess[2, ] <- -c(sum(x * mu), sum(x ^ 2 * mu), sum(x ^ 3 * mu))
+  alg_hess[3, ] <- -c(sum(x ^ 2 * mu), sum(x ^ 3 * mu), sum(x ^ 4 * mu))
+  return(alg_hess)
+}
+
+pois_res <- adjust_loglik(pois_glm_loglik, y = y, x = x, p = 3,
+                          alg_deriv = pois_alg_deriv, alg_hess = pois_alg_hess)
+
+my_ests <- round(attr(pois_res, "MLE"), 4)
+my_ses <- round(attr(pois_res, "SE"), 4)
+my_adj_ses <- round(attr(pois_res, "adjSE"), 4)
+
+test_that("MLEs agree", {
+  testthat::expect_equal(ests, my_ests, tolerance = my_tol)
+})
+test_that("SEs agree", {
+  testthat::expect_equal(ses, my_ses, tolerance = my_tol)
+})
+test_that("adjusted SEs agree", {
+  testthat::expect_equal(adj_ses, my_adj_ses, tolerance = my_tol)
+})
