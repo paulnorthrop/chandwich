37fb1b2d488b6988fd2d73f7f4f42f2452bf172d
Paul Northrop <p.northrop@ucl.ac.uk>
2017-10-21 22:46:54 +0100

    Create plot.confint that can plot a curve for each type of adjustment. [ci skip]

--- R/plot_and_summary.R
+++ R/plot_and_summary.R
@@ -124,34 +124,6 @@ summary.chandwich <- function(object, digits = max(3, getOption("digits")-3),
   return(res)
 }
 
-# ============================== plot.confint =================================
-
-#' Plot diagnostics a confint object
-#'
-#' \code{plot} method for class "confint".
-#'
-#' @param x an object of class "confint", a result of a call to
-#'   \code{\link{conf_intervals}}.
-#' @param y Not used.
-#' @param ... Additional arguments passed on to ...
-#' @return Nothing is returned.
-#' @export
-plot.confint <- function(x, y, ..., add_lines = TRUE) {
-  if (!inherits(x, "confint")) {
-    stop("use only with \"confint\" objects")
-  }
-  my_matplot <- function(x, y, ..., type) {
-    matplot(x, y, type = "l", ...)
-  }
-  my_matplot(x$parameter_vals, x$prof_loglik_vals, ...)
-  if (add_lines) {
-    abline(h = x$cutoff)
-    abline(v = x$prof_CI[, 1])
-    abline(v = x$prof_CI[, 2])
-  }
-  return(invisible())
-}
-
 # ============================== plot.confreg =================================
 
 #' Plot diagnostics a confreg object
@@ -164,7 +136,7 @@ plot.confint <- function(x, y, ..., add_lines = TRUE) {
 #' may be superimposed on the same plot.
 #'
 #' @param x,y,y2,y3 objects of class "confreg", a result of a call to
-#'   \code{\link{conf_region}}.
+#'   \code{\link{conf_region}}.  Contours are plotted for each object.
 #' @param conf A numeric vector of confidence levels, i.e. numbers in
 #'   (0, 100).  A confidence region contour is plotted for each value in
 #'   \code{conf}.
@@ -187,7 +159,7 @@ plot.confreg <- function(x, y = NULL, y2 = NULL, y3 = NULL, conf = 95,
                                         !is.null(y3))),
                          legend_pos = "topleft", ...) {
   if (!inherits(x, "confreg")) {
-    stop("use only with \"confreg\" objects")
+    stop("x must be a \"confreg\" object")
   }
   x_range <- range(x$grid1, y$grid1, y2$grid1, y3$grid1, finite = TRUE)
   y_range <- range(x$grid2, y$grid2, y2$grid2, y3$grid2, finite = TRUE)
@@ -221,24 +193,24 @@ plot.confreg <- function(x, y = NULL, y2 = NULL, y3 = NULL, conf = 95,
   if (is.null(user_args$col)) {
     my_col <- rep(1, 4)
   } else {
-    my_col <- rep(user_args$col, 4)
+    my_col <- rep_len(user_args$col, 4)
   }
   legend_args$col <- my_col
   if (is.null(user_args$lty)) {
     my_lty <- 1:4
   } else {
-    my_lty <- rep(user_args$lty, 4)
+    my_lty <- rep_len(user_args$lty, 4)
   }
   legend_args$lty <- my_lty
   if (is.null(user_args$lwd)) {
     my_lwd <- rep(1, 4)
   } else {
-    my_lwd <- rep(user_args$lwd, 4)
+    my_lwd <- rep_len(user_args$lwd, 4)
   }
   legend_args$lwd <- my_lwd
   # Create plot using x
   max_loglik <- x$max_loglik
-  cutoff <- max_loglik - qchisq(conf  / 100, 2) / 2
+  cutoff <- max_loglik - stats::qchisq(conf  / 100, 2) / 2
   user_args$col <- my_col[1]
   user_args$lty <- my_lty[1]
   user_args$lwd <- my_lwd[1]
@@ -248,8 +220,11 @@ plot.confreg <- function(x, y = NULL, y2 = NULL, y3 = NULL, conf = 95,
   types <- x$type
   # Add to plot using y
   if (!is.null(y)) {
+    if (!inherits(y, "confreg")) {
+      stop("y must be a \"confreg\" object")
+    }
     max_loglik <- y$max_loglik
-    cutoff <- max_loglik - qchisq(conf  / 100, 2) / 2
+    cutoff <- max_loglik - stats::qchisq(conf  / 100, 2) / 2
     user_args$col <- my_col[2]
     user_args$lty <- my_lty[2]
     user_args$lwd <- my_lwd[2]
@@ -260,8 +235,11 @@ plot.confreg <- function(x, y = NULL, y2 = NULL, y3 = NULL, conf = 95,
   }
   # Add to plot using y2
   if (!is.null(y2)) {
+    if (!inherits(y2, "confreg")) {
+      stop("y2 must be a \"confreg\" object")
+    }
     max_loglik <- y2$max_loglik
-    cutoff <- max_loglik - qchisq(conf  / 100, 2) / 2
+    cutoff <- max_loglik - stats::qchisq(conf  / 100, 2) / 2
     user_args$col <- my_col[3]
     user_args$lty <- my_lty[3]
     user_args$lwd <- my_lwd[3]
@@ -272,8 +250,11 @@ plot.confreg <- function(x, y = NULL, y2 = NULL, y3 = NULL, conf = 95,
   }
   # Add to plot using y3
   if (!is.null(y3)) {
+    if (!inherits(y3, "confreg")) {
+      stop("y3 must be a \"confreg\" object")
+    }
     max_loglik <- y3$max_loglik
-    cutoff <- max_loglik - qchisq(conf  / 100, 2) / 2
+    cutoff <- max_loglik - stats::qchisq(conf  / 100, 2) / 2
     user_args$col <- my_col[4]
     user_args$lty <- my_lty[4]
     user_args$lwd <- my_lwd[4]
@@ -281,6 +262,174 @@ plot.confreg <- function(x, y = NULL, y2 = NULL, y3 = NULL, conf = 95,
                           levels = cutoff, add = TRUE), user_args)
     do.call(graphics::contour, for_contour)
     types <- c(types, y3$type)
+  }
+  # Add a legend?
+  if (legend | is.character(legend)) {
+    legend_args$x <- legend_pos
+    if (legend) {
+      legend_args$legend <- types
+    } else {
+      legend_args$legend <- legend
+    }
+    if (is.null(legend_args$title)) {
+      legend_args$title <- "adjustment"
+    }
+    do.call(graphics::legend, legend_args)
+  }
+  return(invisible())
+}
+
+# ============================== plot.confint =================================
+
+# Check that y$which_pars[which_index] = which_par
+# Check that the models x, y, y2, y3 are consistent
+# - similarly in conf_reg_plot? At the moment I calculate separate cutoffs
+
+#' Plot diagnostics a confint object
+#'
+#' \code{plot} method for class "confint".
+#' Plots the (profile) loglikelihood for a parameter using the values
+#' calculated by \code{\link{conf_intervals}}.
+#' Up to 4 different types of loglikelihood (see the argument \code{type}
+#' to the function returned by \code{\link{adjust_loglik}})
+#' may be superimposed on the same plot.
+#' By default (\code{add_lines = TRUE}) the confidence limits calculated
+#' in \code{\link{conf_intervals}} are indicated on the plot .
+#'
+#' @param x,y,y2,y3 objects of class "confint", a result of a call to
+#'   \code{\link{conf_intervals}}.  A (profile) loglikelihood will be
+#'   plotted for each object.
+#' @param which_par A scalar specifying the parameter for which the plot
+#'   is produced.  Can be either a numeric vector, specifying index of the
+#'   component of the \strong{full} parameter vector, or a character scalar
+#'   parameter name.  The former must be in \code{x$which_pars}, the latter
+#'   must be in \code{names(\code{x$which_pars})}.
+#' @param add_lines A logical scalar.  Whether or not to add horizontal lines
+#'   to the plot to identify the confidence limits.
+#' @param conf A numeric vector of values in (0, 100).  If
+#'   \code{add_lines = TRUE} then a horizontal line is added for each
+#'   value in \code{conf}.  If \code{conf} is not supplied then the
+#'   value stored in \code{x$conf} is used.
+#' @param legend A logical scalar or a character vector.  If this is
+#'   supplied then a legend is added to the plot.  If \code{legend} is a
+#'   character vector then it is used as the argument \code{legend}
+#'   to \code{\link[graphics]{legend}}.  Otherwise, i.e. if
+#'   \code{legend = TRUE} then the component \code{type} of the input
+#'   object(s) \code{x, y, y2, y3} are used.
+#' @param legend_pos The position of the legend (if required) specified using
+#'   the argument \code{x} in \code{\link[graphics]{legend}}.
+#' @param ... Additional arguments passed to \code{\link[graphics]{matplot}}
+#'   or \code{\link[graphics]{legend}}.  The arguments \code{col}, \code{lty}
+#'   and \code{lwd} will (in a consistent way) by both
+#'   \code{\link[graphics]{matplot}} and \code{\link[graphics]{legend}}.
+#' @return Nothing is returned.
+#' @export
+plot.confint <- function(x, y = NULL, y2 = NULL, y3 = NULL,
+                         which_par = x$which_pars[1], conf = x$conf,
+                         add_lines = TRUE,
+                         legend = any(c(!is.null(y), !is.null(y2),
+                                        !is.null(y3))),
+                         legend_pos = "topleft", ...) {
+  if (!inherits(x, "confint")) {
+    stop("x must be a \"confint\" object")
+  }
+  if (length(which_par) != 1) {
+    stop("which_par must have length one")
+  }
+  # Where is which_par positioned in x$which_pars
+  if (is.numeric(which_par)) {
+    which_index <- match(which_par, x$which_pars)
+  } else if (is.character(which_par)) {
+    par_names <- names(x$which_pars)
+    which_index <- match(which_par, par_names)
+    which_par <- x$which_pars[which_index]
+  } else {
+    stop("which_par must be numeric or character")
+  }
+  par_name <- names(x$which_pars)[which_index]
+  # Function to extract the desired values from the input objects
+  extract_values <- function(object) {
+    if (which_par %in% object$which_pars) {
+      which_column <- match(which_par, object$which_pars)
+      x_vals <- object$parameter_vals[, which_column]
+      y_vals <- object$prof_loglik_vals[, which_column]
+    } else {
+      stop("Numeric which_par must be in object$which_pars")
+    }
+    return(list(x_vals = x_vals, y_vals = y_vals))
+  }
+  # Pick the correct columns in x$parameter_vals and x$prof_loglik_vals
+  temp <- extract_values(x)
+  x_vals <- temp$x_vals
+  y_vals <- temp$y_vals
+  types <- x$type
+  if (!is.null(y)) {
+    if (!inherits(y, "confint")) {
+      stop("y must be a \"confint\" object")
+    }
+    temp <- extract_values(y)
+    x_vals <- cbind(x_vals, temp$x_vals)
+    y_vals <- cbind(y_vals, temp$y_vals)
+    types <- c(types, y$type)
+  }
+  if (!is.null(y2)) {
+    if (!inherits(y2, "confint")) {
+      stop("y2 must be a \"confint\" object")
+    }
+    temp <- extract_values(y2)
+    x_vals <- cbind(x_vals, temp$x_vals)
+    y_vals <- cbind(y_vals, temp$y_vals)
+    types <- c(types, y2$type)
+  }
+  if (!is.null(y3)) {
+    if (!inherits(y3, "confint")) {
+      stop("y3 must be a \"confint\" object")
+    }
+    temp <- extract_values(y3)
+    x_vals <- cbind(x_vals, temp$x_vals)
+    y_vals <- cbind(y_vals, temp$y_vals)
+    types <- c(types, y3$type)
+  }
+  x_range <- range(x_vals, finite = TRUE)
+  y_range <- range(y_vals, finite = TRUE)
+  # User-supplied arguments.
+  user_args <- list(...)
+  # Always plot lines only
+  user_args$type = "l"
+  l_cond <- names(user_args) %in% methods::formalArgs(graphics::legend)
+  lines_cond <- names(user_args) %in% c("col", "lty", "lwd")
+  legend_args <- user_args[l_cond]
+  user_args <- user_args[!l_cond | lines_cond]
+  # If xlab or ylab are not supplied then use names(x$which_pars), if present
+  if (is.null(user_args$xlab)) {
+    user_args$xlab <- parse(text = par_name)
+  }
+  if (is.null(user_args$ylab)) {
+    user_args$ylab <- "profile loglikelihood"
+  }
+  if (is.null(user_args$xlim)) {
+    user_args$xlim <- x_range
+  }
+  if (is.null(user_args$ylim)) {
+    user_args$ylim <- y_range
+  }
+  if (is.null(user_args$col)) {
+    user_args$col <- rep(1, 4)
+  }
+  legend_args$col <- user_args$col
+  if (is.null(user_args$lty)) {
+    user_args$lty <- 1:4
+  }
+  legend_args$lty <- user_args$lty
+  if (is.null(user_args$lwd)) {
+    user_args$lwd <- rep(1, 4)
+  }
+  legend_args$lwd <- user_args$lwd
+  for_matplot <- c(list(x = x_vals, y = y_vals), user_args)
+  do.call(graphics::matplot, for_matplot)
+  cutoff <- x$max_loglik - stats::qchisq(conf  / 100, 1) / 2
+  if (add_lines) {
+    graphics::abline(h = cutoff)
   }
   # Add a legend?
   if (legend | is.character(legend)) {
