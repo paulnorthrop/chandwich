943c2b7621fe8af5aa83ab2f92ae8eb3031318cb
Paul Northrop <p.northrop@ucl.ac.uk>
2017-11-05 13:38:53 +0000

    More tests for adjust_loglik.

--- tests/testthat/test-adjust_loglik.R
+++ tests/testthat/test-adjust_loglik.R
@@ -68,3 +68,62 @@ test_that("Adj. SEs: cluster and default agree", {
   testthat::expect_equal(adjse_3, adjse_4, tolerance = my_tol)
 })
 
+# Repeat 1 and 2 when using method = "L-BFGS-B"
+
+# Different ways to set the number of parameters p and the cluster indicator
+# Using par_names
+rat_res_1 <- adjust_loglik(loglik = binom_loglik, data = rats, par_names = "p",
+                           method = "L-BFGS-B")
+# Using init
+rat_res_2 <- adjust_loglik(loglik = binom_loglik, data = rats, init = 0.1,
+                           method = "L-BFGS-B")
+
+mle_1 <- as.numeric(attr(rat_res_1, "MLE"))
+mle_2 <- attr(rat_res_2, "MLE")
+se_1 <- as.numeric(attr(rat_res_1, "SE"))
+se_2 <- attr(rat_res_2, "SE")
+adjse_1 <- as.numeric(attr(rat_res_1, "adjSE"))
+adjse_2 <- attr(rat_res_2, "adjSE")
+test_that("MLEs: par_names and init agree, method = L-BFGS-B", {
+  testthat::expect_equal(mle_1, mle_2, tolerance = my_tol)
+})
+test_that("SEs: par_names and init agree, method = L-BFGS-B", {
+  testthat::expect_equal(se_1, se_2, tolerance = my_tol)
+})
+test_that("Adj. SEs: par_names and init agree, method = L-BFGS-B", {
+  testthat::expect_equal(adjse_1, adjse_2, tolerance = my_tol)
+})
+
+# Check that errors are produced when they should be
+
+# Lengths of par_names and init are not consistent
+check_error <- try(adjust_loglik(loglik = binom_loglik, data = rats,
+                                 par_names = "p", init = c(0.1, 0.1)),
+                                 silent = TRUE)
+test_that("Lengths of par_names and init are not consistent", {
+  testthat::expect_identical(class(check_error), "try-error")
+})
+
+# p and length of par_names are not consistent
+check_error <- try(adjust_loglik(loglik = binom_loglik, data = rats, p = 2,
+                                 par_names = "p"),
+                   silent = TRUE)
+test_that("p and length of par_names are not consistent", {
+  testthat::expect_identical(class(check_error), "try-error")
+})
+
+# p and length of init are not consistent
+check_error <- try(adjust_loglik(loglik = binom_loglik, data = rats, p = 1,
+                                 init = c(0.1, 0.1)),
+                   silent = TRUE)
+test_that("p and length of init are not consistent", {
+  testthat::expect_identical(class(check_error), "try-error")
+})
+
+# p and lengths of par_names and init are not consistent
+check_error <- try(adjust_loglik(loglik = binom_loglik, data = rats, p = 2,
+                                 par_names = "p", init = 0.1),
+                   silent = TRUE)
+test_that("p, lengths of par_names and init are not consistent", {
+  testthat::expect_identical(class(check_error), "try-error")
+})
