88cf7b2ad6615155a2444d493d134b57fb75ecc8
Paul Northrop <p.northrop@ucl.ac.uk>
2018-08-29 10:14:46 +0100

    Clarify what happens re approx when smaller is not supplied.

--- R/compare_models.R
+++ R/compare_models.R
@@ -18,6 +18,8 @@
 #' @param approx A logical scalar.  If \code{approx = TRUE} then the
 #'   approximation detailed by equations (18)-(20) of Chandler and Bate (2007)
 #'   is used.  This option is available only if \code{smaller} is supplied.
+#'   If \code{smaller} is not supplied then \code{approx = TRUE} is used.
+#'
 #'   The approximation doesn't make sense if \code{type = "none"}.  If
 #'   \code{type = "none"} and \code{approx = TRUE} then \code{approx} is
 #'   set to \code{FALSE} with no warning.
